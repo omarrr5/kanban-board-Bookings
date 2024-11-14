d615e3021fa28b77b93e960035d9b34144c52a04
omar tarik yehya <khalaf.omar20@gmail.com>
2024-11-14 17:12:29 +0200

    feat: added cors policy for the backend

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Add the CORS middleware
+        $middleware->add(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
