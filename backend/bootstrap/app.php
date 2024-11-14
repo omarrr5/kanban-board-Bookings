bb2a1b0dab28a64f9f99f2333217cb152ca5d49c
omar tarik yehya <khalaf.omar20@gmail.com>
2024-11-14 17:19:59 +0200

    feat: minor changes

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Add the CORS middleware
-        $middleware->add(HandleCors::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use App\Http\Middleware\SkipCorsMiddleware;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::middleware([SkipCorsMiddleware::class])->group(function () {
-    Route::get('/tasks', [TaskController::class, 'index']);
-    Route::get('/tasks/{id}', [TaskController::class, 'show']);
-    Route::post('/tasks', [TaskController::class, 'store']);
-    Route::put('/tasks/{id}', [TaskController::class, 'update']);
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-});
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
